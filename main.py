c08cd5dfa2c90c878dd0bd824b35d48b951b1c00
JuztJet <abdullahlatif098@outlook.com>
2023-08-21 08:08:58 +0100

    Trying to make game adjusts for different screen lengths

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pygame.init()
 
 # Getting width and height of window
 w, h = pygame.display.get_desktop_sizes()[0]
-#w, h = 1300, 700
+#w, h = 700, 400
 icon_image = pygame.image.load("Assets\Stick-Man\Stick Image.png")
 pygame.display.set_icon(icon_image)
 pygame.display.set_caption("Stick-Wacker", "Stick Or is it?")
@@ -18,15 +18,15 @@ clock = pygame.time.Clock()
 running = True
 reset = False
 play = False
-player_font = pygame.font.Font('Assets/RobotoMono-Bold.ttf', 20)
+player_font = pygame.font.Font('Assets/RobotoMono-Bold.ttf', int(w*0.013))
 score_numbers = [0, 0]
 
-logo_images = [pygame.image.load(image) for image in [
+logo_images = [pygame.transform.smoothscale(pygame.image.load(image).convert_alpha(), (int(w*0.326), int(h*0.576))) for image in [
     'Assets/Start Up/Logo1.png',
     'Assets/Start Up/Logo2.png'
 ]]
 
-play_image = pygame.image.load("Assets\Start Up\Play.png")
+play_image = pygame.transform.smoothscale(pygame.image.load("Assets\Start Up\Play.png"), (int(0.207*w), int(0.1689*h)))
 logo_images_rect = [image.get_rect(center=(w / 2, h * 0.35)) for image in logo_images]
 play_image_rect = play_image.get_rect(center=(w / 2, h * .8))
 logo_tick = 0
@@ -50,7 +50,7 @@ def start_screen(logo_frame, logo_tick, play):
 
 
 class Players(pygame.sprite.Sprite):
-    score = player_font.render('{}-{}'.format(score_numbers[0], score_numbers[1]), True, "Black")
+    score = player_font.render('{}-{}'.format(score_numbers[0], score_numbers[1]), False, "Black")
     score_rect = score.get_rect(center=(w / 2, h * .9))
 
     def __init__(self, name, pos):
@@ -96,7 +96,7 @@ class Players(pygame.sprite.Sprite):
              'Assets/Stick-Man/Stick Man Wacking/Stick-Man Wacking/img4.png',
              'Assets/Stick-Man/Stick Man Wacking/Stick-Man Wacking/img5.png']]
 
-        self.player_text = player_font.render(self.name, True, "Blue")
+        self.player_text = player_font.render(self.name, False, "Blue")
         self.attack_time = 0
         if self.name == "Player 1":
             self.image = self.walking_frames[self.walking_frame_num]
@@ -111,12 +111,12 @@ class Players(pygame.sprite.Sprite):
                 'facing_right': True
             }
             self.text_health_control = {
-                'player_health_level': 100,
+                'player_health_level': int(w*0.065),
                 "player_text": self.player_text,
-                "player_text_rect": self.player_text.get_rect(topleft=(self.rect.x, self.rect.y - 100)),
-                "player_health_bar_rect": pygame.Rect((self.rect.x, self.rect.y - 70), (100, 20)),
-                "player_health_bar_background_rect": pygame.Rect((self.rect.x, self.rect.y - 70), (100, 20)),
-                "player_health_bar_outline_rect": pygame.Rect((self.rect.x, self.rect.y - 70), (100, 20))
+                "player_text_rect": self.player_text.get_rect(topleft=(self.rect.x, int(self.rect.y - h*0.115))),
+                "player_health_bar_rect": pygame.Rect((self.rect.x, int(self.rect.y - h*0.08)), (int(w*0.0651), int(h*0.023))),
+                "player_health_bar_background_rect": pygame.Rect((self.rect.x, int(self.rect.y - h*0.08)), (int(w*0.065), int(h*0.023))),
+                "player_health_bar_outline_rect": pygame.Rect((self.rect.x, int(self.rect.y - h*0.08)), (int(w*0.065), int(h*0.023)))
             }
         if self.name == "Player 2":
             self.image = self.reverse_walking_frames[self.walking_frame_num]
@@ -131,12 +131,12 @@ class Players(pygame.sprite.Sprite):
                 'facing_right': False
             }
             self.text_health_control = {
-                'player_health_level': 100,
+                'player_health_level': int(w*0.065),
                 "player_text": self.player_text,
-                "player_text_rect": self.player_text.get_rect(topright=(self.rect.right, self.rect.y - 100)),
-                "player_health_bar_rect": pygame.Rect((self.rect.right - 100, self.rect.y - 70), (100, 20)),
-                "player_health_bar_background_rect": pygame.Rect((self.rect.right - 100, self.rect.y - 70), (100, 20)),
-                "player_health_bar_outline_rect": pygame.Rect((self.rect.right - 100, self.rect.y - 70), (100, 20))
+                "player_text_rect": self.player_text.get_rect(topright=(self.rect.right, self.rect.y - int(h*0.115))),
+                "player_health_bar_rect": pygame.Rect((self.rect.right - int(w*0.065), self.rect.y - int(h*0.081)), (int(w*0.065), int(h*0.023))),
+                "player_health_bar_background_rect": pygame.Rect((self.rect.right - int(w*0.065), self.rect.y - int(h*0.081)), (int(w*0.065), int(h*0.023))),
+                "player_health_bar_outline_rect": pygame.Rect((self.rect.right - int(w*0.065), self.rect.y - int(h*0.081)), (int(w*0.065), int(h*0.023)))
 
             }
 
@@ -174,7 +174,7 @@ class Players(pygame.sprite.Sprite):
         elif self.rect.x >= w + 140:
             self.rect.x = -160
 
-    def move2(self, control):
+    def move2(self, control):#Fix
         direction = pygame.Vector2()
 
         if self.walking_frame_num == 5:
@@ -185,14 +185,17 @@ class Players(pygame.sprite.Sprite):
 
             self.image = self.walking_frames[self.walking_frame_num]
             direction.x += 1
-            self.text_health_control['player_text_rect'] = self.player_text.get_rect(
-                topleft=(self.rect.x + 20, self.rect.y - 105))
-            self.text_health_control['player_health_bar_outline_rect'].update((self.rect.x, self.rect.y - 75),
-                                                                              (100, 20))
-            self.text_health_control['player_health_bar_background_rect'].update((self.rect.x, self.rect.y - 75),
-                                                                                 (100, 20))
-            self.text_health_control['player_health_bar_rect'].update((self.rect.x, self.rect.y - 75), (
-                self.text_health_control['player_health_level'], 20))
+            self.text_health_control['player_text_rect'].x = self.rect.x
+            self.text_health_control['player_text_rect'].update((self.rect.x, int(self.rect.y - h*0.12152)), (self.player_text.get_width(), self.player_text.get_height()))
+            # self.text_health_control['player_text_rect'].y = int(self.rect.y - h*0.12152)
+
+                # topleft=(self.rect.left + 105, int(self.rect.y - h*0.12152)))
+            self.text_health_control['player_health_bar_outline_rect'].update((self.rect.x, self.rect.y - int(h*0.086)),
+                                                                              (int(w*0.065), int(h*0.023)))
+            self.text_health_control['player_health_bar_background_rect'].update((self.rect.x, self.rect.y - int(h*0.086)),
+                                                                                 (int(w*0.065), int(h*0.023)))
+            self.text_health_control['player_health_bar_rect'].update((self.rect.x, self.rect.y - int(h*0.086)), (
+                self.text_health_control['player_health_level'], int(h*0.023)))
         if control == "left":
             self.walking_frame_num = self.walking_tick // 3
 
@@ -214,13 +217,13 @@ class Players(pygame.sprite.Sprite):
         self.walking_tick += 1
 
         if control == "jump":
-            self.rect.y -= 450
-            self.text_health_control['player_text_rect'].y -= 450
-            self.text_health_control['player_health_bar_rect'].y -= 450
-            self.text_health_control['player_health_bar_background_rect'].y -= 450
-            self.text_health_control['player_health_bar_outline_rect'].y -= 450
+            self.rect.y -= h*0.55
+            self.text_health_control['player_text_rect'].y -= h*0.55
+            self.text_health_control['player_health_bar_rect'].y -= h*0.55
+            self.text_health_control['player_health_bar_background_rect'].y -= h*0.55
+            self.text_health_control['player_health_bar_outline_rect'].y -= h*0.55
 
-    def collision_blocker(self):
+    def collision_blocker(self):#Fix
         if player1.rect.colliderect(player2.rect):
             if self.name == "Player 1":
                 if player1.rect.x <= player2.rect.x:
@@ -301,7 +304,7 @@ class Players(pygame.sprite.Sprite):
 
         self.attack_tick += 1
 
-    def damage(self):
+    def damage(self):#Fix
 
         if self.name == "Player 1":
             if player1.rect.colliderect(player2.rect) or player1.control['facing_right'] and (
